840f8909d629c5ac492cf2445cb0ae1cbb916f54
Valo27 <vforvalo@gmail.com>
2025-03-15 15:24:00 +0300

    install makefile and create class Cli

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,23 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    /**
+     * Метод для приветствия пользователя
+     */
+    public static void greetUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+
+        System.out.print("May I have your name? ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
+    }
+}
+
